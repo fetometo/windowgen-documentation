3b09eb9f11587874105ac3cc00109911882bdfdc
fetometo <fetometo@yandex.ru>
2024-06-23 17:34:42 +0300

    theme changed

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'recommonmark',
+    'sphinx_press_theme',
 ]
 
 intersphinx_mapping = {
